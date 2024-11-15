7950325b8a2014bfd10ba02728873296da225c26
ClaudiaVFer <claudia.kiku@hotmail.com>
2024-11-15 13:02:34 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MusicaModule } from './musica/musica.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { UsuariosService } from './usuarios/usuarios.service';
+import { MusicaService } from './musica/musica.service';
+import { MusicaController } from './musica/musica.controller';
+import { UsuariosController } from './usuarios/usuarios.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MusicaModule, UsuariosModule],
+  controllers: [AppController, MusicaController, UsuariosController],
+  providers: [AppService, UsuariosService, MusicaService],
 })
 export class AppModule {}
